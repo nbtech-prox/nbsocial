0f0310f58db619b9671e208bdc2ea2ad7710d351
Nuno Batista - NBTech <nbtech.prox@gmail.com>
2025-02-18 11:21:05 +0000

    Limpando projeto

--- recreate_db.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS nbsocial;
-CREATE DATABASE nbsocial CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL PRIVILEGES ON nbsocial.* TO 'nbsocial'@'localhost';
--- setup_database.sql
+++ setup_database.sql
@@ -1,4 +1,12 @@
+-- Remover banco de dados se existir
+DROP DATABASE IF EXISTS nbsocial;
+
+-- Criar banco de dados com suporte a UTF-8
 CREATE DATABASE nbsocial CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'nbsocial'@'localhost' IDENTIFIED BY 'nbsocial123';
+
+-- Criar usuário se não existir
+CREATE USER IF NOT EXISTS 'nbsocial'@'localhost' IDENTIFIED BY 'nbsocial123';
+
+-- Conceder privilégios
 GRANT ALL PRIVILEGES ON nbsocial.* TO 'nbsocial'@'localhost';
 FLUSH PRIVILEGES;
